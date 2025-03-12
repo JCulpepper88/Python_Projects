0079fd57651aa3d366c3a08e8a32e72a5067ca65
JCulpepper88 <jculpepper88@gmail.com>
2025-03-11 20:23:38 -0500

    Manual binary calculation added

--- Converting to Binary.py
+++ Converting to Binary.py
@@ -1,3 +1,15 @@
 num = 10
 binary_representation = bin(num)[2:]
 print(binary_representation)
+
+
+def decimal_to_binary(n):
+    binary = ""
+    while n > 0:
+        remainder = n % 2  # Get remainder when divided by 2 (either 0 or 1)
+        binary = str(remainder) + binary  # Append remainder to the left
+        n = n // 2  # Divide number by 2 and take the integer part
+    return binary
+
+# Example:
+print(decimal_to_binary(15))  # Output: '1010'
