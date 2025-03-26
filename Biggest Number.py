d6d282233b56324e1d6ee4684a955bd79a264498
JCulpepper88 <jculpepper88@gmail.com>
2025-03-26 08:51:29 -0500

    Starting new function to find biggest number.

--- Biggest Number.py
+++ Biggest Number.py
@@ -1,1 +1,12 @@
-def biggest_number():
+numbers = [5, 7, 2, 8, 1, 9, 3]
+biggest = max(numbers)
+print(biggest)
+
+
+numbers = [4, 6, 1, 7, 3, 9, 8, 13, 2]
+number = numbers[0]
+for i in numbers:
+    if i > number:
+        number = i
+index = numbers.index(number)
+print(f"The highest number in the list is '{number}', which is at location '{index}'")
